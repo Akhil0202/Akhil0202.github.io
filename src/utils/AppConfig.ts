fcba1392ef54eaaf6458ffe030642b3536117f33
Akhil0202 <66013822+Akhil0202@users.noreply.github.com>
2024-03-09 22:05:56 +0530

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
+  site_name: 'D4RK',
   title: 'Astro boilerplate',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  author: 'Akhil',
   locale_region: 'en-us',
   locale: 'en',
 };
