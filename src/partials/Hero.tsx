ab8145f5f27ed3b1a6fbf395a33b72748252315a
Akhil0202 <66013822+Akhil0202@users.noreply.github.com>
2024-03-10 12:40:51 +0530

    Update Hero.tsx

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => (
       avatar={
         <img
           className="h-80 w-64"
-          src="/assets/images/avatar.svg"
+          src="/assets/images/pic.jpg"
           alt="Avatar image"
           loading="lazy"
         />
