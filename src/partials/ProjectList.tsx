73b1d73b1d8d15ae02f1ac3ffe7488e3f79e8f93
Akhil0202 <66013822+Akhil0202@users.noreply.github.com>
2024-03-12 11:32:05 +0530

    Update ProjectList.tsx

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -1,4 +1,4 @@
-/*import {
+import {
   ColorTags,
   GradientText,
   Project,
@@ -17,10 +17,8 @@ const ProjectList = () => (
     <div className="flex flex-col gap-6">
       <Project
         name="Project 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
+        description="Side-channel analysis, finding out AES encryption keys using power analysis."
+        link="/projects/side channel.md"
         img={{
           src: '/assets/images/project-web-design.png',
           alt: 'Project Web Design',
@@ -36,9 +34,7 @@ const ProjectList = () => (
       />
       <Project
         name="Project 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        description="Development of couple of simple android application."
         link="/"
         img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
         category={
@@ -51,9 +47,7 @@ const ProjectList = () => (
       />
       <Project
         name="Project 3"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
+        description="Pneumonia detection using machine learning."
         link="/"
         img={{ src: '/assets/images/project-maps.png', alt: 'Project Maps' }}
         category={
@@ -69,4 +63,4 @@ const ProjectList = () => (
 );
 
 export { ProjectList };
-*/
+
